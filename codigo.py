69dde1b86b1054fc9eb7684a81935546f4bad11e
Almir Cavalcante <almir2cavalcante@gmail.com>
2025-12-02 00:12:08 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,1 +1,11 @@
 import sqlite3
+
+conexao = sqlite3.connect("banco.db")
+cursor = conexao.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS contas_bancarias (
+              id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+              titular TEXT NOT NULL,
+              saldo FLOAT NOT NULL,
+              cpf TEXT NOT NULL UNIQUE,
+              )""")
